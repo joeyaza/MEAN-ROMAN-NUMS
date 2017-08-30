68a2847597873795de84c7b72ad3e76d3670fe25
joeyaza <JAzadehdel@gmail.com>
2017-08-30 08:12:23 +0100

    update, error handling working with number and roman numeral on front end

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -8,17 +8,17 @@ app.controller('Main', ['$scope', '$http', function ($scope, $http) {
 	});
 
 	$scope.submit = function() {
-		if(!/^M*(?:D?C{0,3}|C[MD])(?:L?X{0,3}|X[CL])(?:V?I{0,3}|I[XV])$/i.test($scope.from)) {
-			$scope.error = 'Please enter a valid roman numeral!';
-			$scope.answer = '';
-			return;
-		} else {
+		if(/^M*(?:D?C{0,3}|C[MD])(?:L?X{0,3}|X[CL])(?:V?I{0,3}|I[XV])$/i.test($scope.from) || /^\d+$/i.test($scope.from)) {
 			$scope.error = '';
 			conversionData = $scope.from;
 			$http.post("http://localhost:3001/conversions", {from:conversionData}, {headers: {'Content-Type': 'application/json'} })
 		        .then(function (response) {
-		            $scope.answer = response.data.to;
+		    	$scope.answer = response.data.to;
 		    });
+		} else {
+			$scope.error = 'Please enter a valid roman numeral or number!!';
+			$scope.answer = '';
+			return
 		}
 	};
 }]);
